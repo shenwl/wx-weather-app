d2dc7e4ee1470f19df409a51c693c9068a0d516c
shenwl <shenweilun96@gmail.com>
2018-06-25 10:02:02 +0800

    base page and data

---
+++ pages/index/index.js
@@ -0,0 +1,55 @@
+const weatherMap = {
+  'sunny': '晴天',
+  'cloudy': '多云',
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+}
+
+const weatherColorMap = {
+  'sunny': '#cbeefd',
+  'cloudy': '#deeef6',
+  'overcast': '#c6ced2',
+  'lightrain': '#bdd5e1',
+  'heavyrain': '#c5ccd0',
+  'snow': '#aae1fc'
+}
+
+const nowBaseUrl = 'https://test-miniprogram.com/api/weather/now'
+const futureBaseUrl = 'https://test-miniprogram.com/api/weather/future'
+
+Page({
+  onLoad() {
+    this.getNow()
+  },
+  data: {
+    nowTemp: '',
+    nowWeather: ''
+  },
+  onPullDownRefresh() {
+    console.log('refresh')
+    this.getNow()
+    wx.stopPullDownRefresh()
+  },
+  getNow() {
+    wx.request({
+      url: nowBaseUrl,
+      data: { city: '广州市' },
+      success: (res) => {
+        console.log(res.data)
+        const tempData = res.data.result
+
+        this.setData({
+          nowTemp: tempData.now.temp + '°',
+          nowWeather: weatherMap[tempData.now.weather]
+        })
+
+        wx.setNavigationBarColor({
+          frontColor: '#000000',
+          backgroundColor: weatherColorMap[tempData.now.weather]
+        })
+      }
+    })
+  },
+})
