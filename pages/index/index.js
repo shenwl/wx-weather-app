5db4e3103858087f6af3f2cb0a58808c31a7ac98
shenwl <shenweilun96@gmail.com>
2018-06-26 17:27:24 +0800

    forecast

--- pages/index/index.js
+++ pages/index/index.js
@@ -16,6 +16,23 @@ const weatherColorMap = {
   'snow': '#aae1fc'
 }
 
+const weatherIconUrlMap = {
+  'cloudy': '/images/cloudy-icon.png',
+  'heavyrain': '/images/heavyrain-icon.png',
+  'lightrain': '/images/lightrain-icon.png',
+  'overcast': '/images/cloudy-icon.png',
+  'snow': '/images/snow-icon.png',
+  'sunny': '/images/sunny-icon.png',
+}
+const weatherBgUrlMap = {
+  'cloudy': '/images/cloudy-bg.png',
+  'heavyrain': '/images/heavyrain-bg.png',
+  'lightrain': '/images/lightrain-bg.png',
+  'overcast': '/images/overcast-bg.png',
+  'snow': '/images/snow-bg.png',
+  'sunny': '/images/sunny-bg.png',
+}
+
 const nowBaseUrl = 'https://test-miniprogram.com/api/weather/now'
 const futureBaseUrl = 'https://test-miniprogram.com/api/weather/future'
 
@@ -35,22 +52,49 @@ Page({
       url: nowBaseUrl,
       data: { city: '广州市' },
       success: (res) => {
-        console.log(res.data)
         const tempData = res.data.result
 
         this.setData({
           nowTemp: tempData.now.temp + '°',
-          nowWeather: weatherMap[tempData.now.weather]
+          nowWeather: weatherMap[tempData.now.weather],
+          nowBackgroundImage: weatherBgUrlMap[tempData.now.weather]
         })
-
         wx.setNavigationBarColor({
           frontColor: '#000000',
           backgroundColor: weatherColorMap[tempData.now.weather]
         })
+
+        const forecast = tempData.forecast
+        console.log(forecast)
+        let nowHour = new Date().getHours()
+        let hourlyWeather = []
+        for(let i = 0; i < 24; i += 3) {
+          hourlyWeather.push({
+            time: (i + nowHour) % 24 + '时',
+            iconPath: weatherIconUrlMap[forecast[i / 3].weather],
+            temp: forecast[i / 3].weather.temp
+          })
+        }
+        this.setData({ forecastWeather: hourlyWeather })
+
       },
       complete: () => {
         callback && callback()
       }
     })
   },
+  getFuture() {
+    wx.request({
+      url: futureBaseUrl,
+      data: {
+        city: '广州市',
+        time: Date.parse(new Date()),
+      },
+      success: (res) => {
+        const data = res.data.result
+        this.setData({futureWeather: data})
+      }
+    })
+  },
+
 })
